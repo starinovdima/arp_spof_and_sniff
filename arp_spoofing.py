6b5403659bed55ea42434052342b7f65523fbd88
Dmitry <Dmitry>
2022-11-11 00:51:35 +0300

    ip-mac

--- arp_spoofing.py
+++ arp_spoofing.py
@@ -3,6 +3,7 @@ import socket as sock
 import struct
 import subprocess
 import time
+import ipaddress
 
 from prettytable import PrettyTable
 from socket import socket, AF_INET, SOCK_DGRAM
@@ -27,8 +28,11 @@ def get_gateway_ip():
     if ip_route.isdigit():
         return ip_route
     else:
-        sock_ = sock.gethostbyname(ip_route)
-        return sock_
+        try:
+            sock_ = sock.gethostbyname(ip_route)
+            return sock_
+        except BaseException:
+            return os.popen("ip r | grep default").read().split()[2]
 
 
 def output_mac_ip_table(mac_ip_list):
@@ -41,44 +45,55 @@ def output_mac_ip_table(mac_ip_list):
 
 
 def get_ip_mac_addr(ip):
+
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1,
-                              verbose=False)[0]
+    answered_list = scapy.srp(arp_request_broadcast,iface='wlan0', timeout=3, verbose=False)[0]
+
     clients_list = []
+
     for element in answered_list:
         client_dict = [element[1].psrc, element[1].hwsrc]
+        print(f"ip:{element[1].psrc} mac:{element[1].hwsrc}")
         clients_list.append(client_dict)
     return clients_list
 
 
 def generate_ip(local_ip, prefix):
-    return f'{local_ip.split(".")[0]}.{local_ip.split(".")[1]}.{local_ip.split(".")[2]}.1' + prefix
+    return f'{local_ip.split(".")[0]}.{local_ip.split(".")[1]}.{local_ip.split(".")[2]}.' + str(prefix)
 
 
 # def update_ip_table():
 #   while True:
 
 def get_prefix(local_ip):
-    return '/' + os.popen(f"ip a | zgrep {local_ip}").read().split()[1].split("/")[1]
-
-
+    prefix = 0
+    netmask = os.popen(f"ifconfig | grep {local_ip}").read().split()[3].split(".")
+    for i in range(len(netmask)):
+        prefix += str(bin(int(netmask[i]))).count("1")
+    return '/' + str(prefix)
 def main():
-    if not os.getuid() == 0:
-        print("\n----------- Please, run with SUDO ! -----------")
-        return
+    #if not os.getuid() == 0:
+      #  print("\n----------- Please, run with SUDO ! -----------")
+      #  return
+    print("----- Choose interface ( default: wlan0) ----- ")
+    interface = input("---> ")
+    if ( len(interface) == 0):
+        interface = "wlan0"
     local_ip = get_local_ip()
     gateway_ip = get_gateway_ip()
     prefix = get_prefix(local_ip)
+    #print(generate_ip(local_ip))
     while True:
-        print(f"\n----------- Your local IP is  -->  {local_ip} ----------- ")
+        print(f"\n----------- Your local IP is  -->  {local_ip+prefix} ----------- ")
         print(f"----------- Gateway  IP  is   -->  {gateway_ip}  -----------")
-        mac_ip_list = get_ip_mac_addr(generate_ip(local_ip, prefix))
+        for i in (0,126):
+            mac_ip_list = get_ip_mac_addr(generate_ip(local_ip,126))
         if not (len(mac_ip_list) == 0):
             output_mac_ip_table(mac_ip_list)
         print("\n- Please select target ip or update table(enter 'up') -")
-        answer = input()
+        answer = input("---> ")
         if not "up" in answer:
             break
 
